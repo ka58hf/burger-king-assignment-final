d124fd699a1f795ff5b90efebdfdeef44e817190
ka58hf <102793003+ka58hf@users.noreply.github.com>
2022-04-01 09:21:28 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+//conecting my buttons from css to javascript//
+button.id = "button";
+ button.id = ".button1";
+ button.id = ".btn2";
+ button.id = ".btn3";
+//conecting my images from css to javascript//
+ img.id = ".img2";
+ img.id = ".img3";
+ img.id = ".img4";
+ img.id = ".img5";
+ nav.id = "nav";
